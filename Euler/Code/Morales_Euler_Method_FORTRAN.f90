d63bf843a3e329945e03a17729468d5da4c7569d
JulioM1823 <jmmorale@nmsu.edu>
2023-03-27 14:30:08 -0600

    Fixed energy and made a single output txt file

--- Euler/Code/Morales_Euler_Method_FORTRAN.f90
+++ Euler/Code/Morales_Euler_Method_FORTRAN.f90
@@ -9,12 +9,12 @@ program orbit
     integer, parameter :: ivxp = 1, ivyp = 2, ivzp = 3
     integer, parameter :: ixstar = 1, iystar = 2, izstar = 3
     integer, parameter :: ivxstar = 1, ivystar = 2, ivzstar = 3
-    real*10, parameter :: t_max=100, dt=1e-3
-    real*10, parameter :: G = 1.0, pi=3.14159265358979323846264338327950d0
+    double precision, parameter :: t_max=100, dt=1e-3
+    double precision, parameter :: G = 1.0, pi=3.14159265358979323846264338327950d0
+    double precision :: m(n), mtot, euler_orbit
     character(len=45), parameter :: path = 'C:/Users/julio/Downloads/Hydrodynamics/Euler/'
     integer :: nsteps
-    real*10 :: m(n), mtot, euler_orbit
-
+    
     ! Define masses
     m(iplanet-1) = 3e-6
     m(istar+1) = 1- m(iplanet-1)
@@ -24,7 +24,7 @@ program orbit
     nsteps = int(t_max/dt)
 
     ! Compute 1 the error on 1 orbit
-    euler_orbit = sigE()
+    euler_orbit = euler() 
 
 contains
 
@@ -37,15 +37,15 @@ function initialise() result(rv)
     !   None
     !
     ! OUTPUTS:
-    !   rv(2,n,dim):  Array containing position and velocity vectors.
+    !   [rv(2,n,dim); double precision]:  Array containing position and velocity vectors.
     !
     ! AUTHOR:
     !   Julio M. Morales
     ! --------------------------------------------------------
     implicit none
-    real*10 :: r(n,dim), v(n,dim), rv(n,n,dim)
+    double precision :: r(n,dim), v(n,dim), rv(2,n,dim)
 
-    ! Set positions and velocities
+    ! Set initial positions and velocities
     r(istar, ixstar) = 0.0
     r(istar, iystar) = 0.0
     r(istar, izstar) = 0.0
@@ -74,18 +74,18 @@ function updatePosVel(r, v, a) result(rv_ud)
     !   Update the position and velocity of the masses.
     !
     ! INPUTS:
-    !   [r(3); real*10]:  Position vector.
-    !   [v(3); real*10]:  Velocity vector.
-    !   [a(3); real*10]:  Acceleration vector.
+    !            [r(dim); double precision]:  Position vector.
+    !            [v(dim); double precision]:  Velocity vector.
+    !            [a(dim); double precision]:  Acceleration vector.
     !
     ! OUTPUTS:
-    !      [rv_ud(3,2)]:  Array containing position and velocity vectors.
+    !      [rv_ud(dim,n); double precision]:  Array containing position and velocity vectors.
     !
     ! AUTHOR:
     !   Julio M. Morales
     ! --------------------------------------------------------
     implicit none
-    real*10 :: r(dim), v(dim), a(dim), rv_ud(dim,n)
+    double precision :: r(dim), v(dim), a(dim), rv_ud(dim,n)
     
     ! Update position and velocity
     r = r + v*dt
@@ -103,18 +103,18 @@ function computeAccel(m_dum, r, j) result(a)
     !   Compute the acceleration of the masses.
     !
     ! INPUTS:
-    !       [m_dum; real*10]:  Mass of jth body.
-    !   [r(n, dim); real*10]:  Position vector.
-    !           [j; integer]:  Index of the jth object.
+    !         [m_dum; double precision]:  Mass of jth body.
+    !     [r(n, dim); double precision]:  Position vector.
+    !             [j; integer]:  Index of the jth object.
     !
     ! OUTPUTS:
-    !        [a(3); real*10]:  Acceleration vector.
+    !        [a(dim); double precision]:  Acceleration vector.
     !
     ! AUTHOR:
     !   Julio M. Morales
     ! --------------------------------------------------------
     implicit none
-    real*10 :: m_dum, r(n, dim), r_squared, r_cubed, r_diff(dim), a(dim)
+    double precision :: m_dum, r(n, dim), r_squared, r_cubed, r_diff(dim), a(dim)
     integer :: i, j
 
     ! Initialize r_diff
@@ -142,68 +142,7 @@ function computeAccel(m_dum, r, j) result(a)
 
 end function computeAccel
 
-function computeEnergy(r, v) result(E)
-    ! --------------------------------------------------------
-    ! PURPOSE:
-    !   Compute the energy of the system.
-    !
-    ! INPUTS:
-    !       [r(n, dim); real*10]:  Position vector.
-    !       [v(n, dim); real*10]:  Velocity vector.
-    !
-    ! OUTPUTS:
-    !       [E; real*10]:  Energy of the system.
-    !
-    ! AUTHOR:
-    !   Julio M. Morales
-    ! --------------------------------------------------------
-    implicit none
-    real*10 :: r(n, dim), v(n, dim), E
-
-    ! Compute energy
-    E = 0.5*(m(1)*dot_product(v(1,:), v(1,:)) + m(2)*dot_product(v(2,:), v(2,:))) - m(1)*m(2)/norm2(r(2,:) - r(1,:))
-
-end function computeEnergy
-
-function computeAngularMomentum(r, v) result(L)
-    ! --------------------------------------------------------
-    ! PURPOSE:
-    !   Compute the angular momentum of the system.
-    !
-    ! INPUTS:
-    !   [r(n, dim); real*10]:  Position vector.
-    !   [v(n, dim); real*10]:  Velocity vector.
-    !
-    ! OUTPUTS:
-    !      [L(dim); real*10]:  Angular momentum of the system.
-    !
-    ! AUTHOR:
-    !   Julio M. Morales
-    ! --------------------------------------------------------
-    implicit none
-    real*10 :: r(n, dim), v(n, dim), rDiff(dim), vDiff(dim), rr(dim), phi(dim), vrad(dim), vphi(dim), phidot(dim), L(dim)
-    
-    ! Define rDiff and vDiff
-    rDiff = r(iplanet,:) - r(istar,:)
-    vDiff = v(iplanet,:) - v(istar,:)
-
-    ! Converting the coordinates from Cartesian to Cylindrical ####
-    rr  = sqrt(rDiff(istar)**2 + rDiff(iplanet)**2)
-    phi = atan2(rDiff(iplanet), rDiff(istar))
-
-    ! Velocity
-    vrad = vDiff(istar)*cos(phi) + vDiff(iplanet)*sin(phi)
-    vphi = -vDiff(istar)*sin(phi) + vDiff(iplanet)*cos(phi)
-
-    ! Time derivative of phi
-    phidot = vphi/rr
-
-    ! Calculate the angular momentum
-    L = (rr**2)*(phidot)
-
-end function computeAngularMomentum
-
-function sigE() result(sig_E)
+function euler() result(res)
 
     ! --------------------------------------------------------
     ! PURPOSE:
@@ -213,34 +152,29 @@ function sigE() result(sig_E)
     !   None
     !
     ! OUTPUTS:
-    !   [sig_E; real*10]: Relative error in energy.
+    !         Text file containing position and velocity data of both bodies.
     !
     ! AUTHOR:
     !   Julio M. Morales
     ! --------------------------------------------------------
     implicit none
     integer :: i, j
-    real*10 ::  E, E0, sig_E, r(n,dim), v(n,dim), a(dim), rv(n,n,dim), rv_ud(dim, n), t(nsteps) 
+    double precision ::  r(n,dim), v(n,dim), a(dim), rv(n,n,dim), rv_ud(dim, n), t(nsteps), res
 
     ! File saving stuff
-    real*10 :: xplanet(nsteps), yplanet(nsteps), zplanet(nsteps), xstar(nsteps), ystar(nsteps), zstar(nsteps)
-    real*10 :: vxplanet(nsteps), vyplanet(nsteps), vzplanet(nsteps), vxstar(nsteps), vystar(nsteps), vzstar(nsteps)
-    real*10 :: En_error(nsteps)
-    character(len=65) :: filename1, filename2, filename5
-    character(len=68) :: filename3, filename4
-    character(len=7)  :: col_names1(4) = (/'time(n)', 'xplanet', 'yplanet', 'zplanet'/)
-    character(len=5)  :: col_names2(3) = (/'xstar', 'ystar', 'zstar' /)
-    character(len=8)  :: col_names3(3) = (/'vxplanet', 'vyplanet', 'vzplanet'/)
-    character(len=6)  :: col_names4(3) = (/'vxstar', 'vystar', 'vzstar' /)
-    character(len=10) :: col_names5(1) = (/'energy_err' /)
+    double precision  :: xplanet(nsteps), yplanet(nsteps), zplanet(nsteps), xstar(nsteps), ystar(nsteps), zstar(nsteps)
+    double precision  :: vxplanet(nsteps), vyplanet(nsteps), vzplanet(nsteps), vxstar(nsteps), vystar(nsteps), vzstar(nsteps)
+    character(len=67) :: filename
+    character(len=8) :: columns(9) = &
+    (/'  time  ', ' xplanet', ' yplanet', &
+      '  xstar ', '  ystar ', 'vxplanet', &
+      'vyplanet', ' vxstar ', ' vystar '/)
+
 
     ! Declare the initial position and velocity for both the Sun and the Earth
     rv = initialise()
     r  = rv(1,:,:)
     v  = rv(2,:,:)
-    
-    ! Declare the initial energy
-    E0 = computeEnergy(r, v)
 
     ! Initialize the time array
     do i = 1, nsteps
@@ -282,81 +216,29 @@ function sigE() result(sig_E)
             v(j,:) = rv_ud(:,2)
 
         end do
-        
-        ! Check if less than 1 orbit has been completed
-        if (t(i) >= 2*pi) then
-            
-            ! Get the current energy
-            E = computeEnergy(r, v)
-
-            ! Compute the relative error in the energy
-            sig_E = abs((E - E0)/E0)
-            En_error(i) = sig_E
-
-        end if
 
     end do
 
     ! Ask user for file name
-    filename1 = path // 'Morales_Euler_rp.txt'
-    filename2 = path // 'Morales_Euler_vp.txt'
-    filename3 = path // 'Morales_Euler_rstar.txt'
-    filename4 = path // 'Morales_Euler_vstar.txt'
-    filename5 = path // 'Morales_Euler_En.txt'
+    filename = path // 'Morales_Euler.txt'
 
     ! Open file for writing, with column names as first row
-    open(unit = 1, file = filename1, status = 'replace', action = 'write')
-    do i = 1, size(col_names1)
-        write(1, '(A15, A15)', advance = "no") ' ', col_names1(i)
+    open(unit = 1, file = filename, status = 'replace', action = 'write')
+    do i = 1, size(columns)
+        write(1, '(A15, A15)', advance = "no") ' ', columns(i)
     end do
     write(1,*)
-
-    ! Open file for writing, with column names as first row
-    open(unit = 2, file = filename2, status = 'replace', action = 'write')
-    do i = 1, size(col_names2)
-        write(2, '(A15, A15)', advance = "no") ' ', col_names2(i)
-    end do
-    write(2,*)
-
-    ! Open file for writing, with column names as first row
-    open(unit = 3, file = filename3, status = 'replace', action = 'write')
-    do i = 1, size(col_names3)
-        write(3, '(A15, A15)', advance = "no") ' ', col_names3(i)
-    end do
-    write(3,*)
-
-    ! Open file for writing, with column names as first row
-    open(unit = 4, file = filename4, status = 'replace', action = 'write')
-    do i = 1, size(col_names4)
-        write(4, '(A15, A15)', advance = "no") ' ', col_names4(i)
-    end do
-    write(4,*)
-
-    ! Open file for writing, with column names as first row
-    open(unit = 5, file = filename5, status = 'replace', action = 'write')
-    do i = 1, size(col_names5)
-        write(5, '(A15, A15)', advance = "no") ' ', col_names5(i)
-    end do
-    write(5,*)
     
     ! Loop through each row of data and write to each respective column
     do i=1, nsteps
-        write(1, '(4F30.10)') t(i), xplanet(i), yplanet(i), zplanet(i)
-        write(2, '(3F30.10)') xstar(i), ystar(i), zstar(i)
-        write(3, '(3F30.10)') vxplanet(i), vyplanet(i), vzplanet(i)
-        write(4, '(3F30.10)') vxstar(i), vystar(i), vzstar(i)
-        write(5, '(1F30.10)') En_error(i)
+        write(1, '(9F30.16)') t(i), xplanet(i), yplanet(i), xstar(i), ystar(i), vxplanet(i), vyplanet(i), vxstar(i), vystar(i)
     end do
      
     ! Close file
     close(1)
-    close(2)
-    close(3)
-    close(4)
-    close(5)
-     
     print *, "Simulation Complete!"
+    res = 0.0
 
-end function sigE
+end function euler
 
 end program orbit
